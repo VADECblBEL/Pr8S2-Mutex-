bf95d4ef2f92beb798807bc77f38a1b041d756d4
Вадим <vadimmm.m@ya.ru>
2020-10-25 15:15:25 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Mutex oneMutex = null;
+        const String MutexName = "RUNMEONLYONCE";
+        try
+        {
+            oneMutex = Mutex.OpenExisting(MutexName);
+        }
+        catch (WaitHandleCannotBeOpenedException)
+        {
+
+        }
+        if (oneMutex == null)
+        {
+            oneMutex = new Mutex(true, MutexName);
+        }
+        else
+        {
+
+            oneMutex.Close();
+            return;
+        }
+        Console.WriteLine("Our Application");
+
+    }
+}
